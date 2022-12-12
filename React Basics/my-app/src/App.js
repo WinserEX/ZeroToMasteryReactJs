ac0a13f0757a2a76490ff7c7184870271fc24388
WinserEX <winser500@gmail.com>
2022-12-11 23:54:39 -0400

    Will add fetch in order to get mock data from an API, will also serve as an explanation for componentDidMount and React's lifecycle

--- React Basics/my-app/src/App.js
+++ React Basics/my-app/src/App.js
@@ -2,35 +2,36 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  //Runs first
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-        },
-        {
-          name: 'Frank',
-        },
-        {
-          name: 'Jacky',
-        },
-        {
-          name: 'Winser',
-        }
-       ]       
+      monsters: [],       
     };
   }
 
+  //Runs 3rd
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(()=> {
+        return {monsters: users}
+          },
+          () => {
+            console.log(this.state);
+          }
+        ))
+  }
+  //Runs second, then runs 4th
    render() { 
       return (
           <div className="App">
-          {
-            this.state.monsters.map((monster) => {
-              return <h1>{monster.name}</h1>
-            })
-          }
+            {
+              this.state.monsters.map((monster) => {
+                return <div key={monster.id}><h1>{monster.name}</h1></div>
+              })
+            }
           </div>
       );
     }

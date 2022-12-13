898dc19abbd41f181bc42eabdc8e0550787818b0
WinserEX <winser500@gmail.com>
2022-12-12 23:01:48 -0400

    Will add search functionality to app, implemented several optimizations in order to make app more efficient

--- React Basics/my-app/src/App.js
+++ React Basics/my-app/src/App.js
@@ -7,12 +7,16 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [],       
+      monsters: [],
+      searchField: '',
+      test: ''       
     };
+    console.log('constructor')
   }
 
   //Runs 3rd
   componentDidMount(){
+    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) => this.setState(()=> {
@@ -21,14 +25,36 @@ class App extends Component {
           () => {
             console.log(this.state);
           }
-        ))
+        ))    
   }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(()=>{
+      return { searchField };
+    })              
+  }
+
   //Runs second, then runs 4th
-   render() { 
+   render() {
+    console.log('render');
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
       return (
           <div className="App">
+            <input 
+            className='search-box' 
+            type='search' 
+            placeholder='search monsters' 
+            onChange = {onSearchChange}/>
             {
-              this.state.monsters.map((monster) => {
+              filteredMonsters.map((monster) => {
                 return <div key={monster.id}><h1>{monster.name}</h1></div>
               })
             }

e54dac2cbc98620cac8b9474c795677992806c6d
WinserEX <winser500@gmail.com>
2022-12-23 17:45:26 -0400

    test

--- React Basics/my-app/src/App.js
+++ React Basics/my-app/src/App.js
@@ -1,64 +1,91 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Cardlist from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
-class App extends Component {
-  //Runs 1st
-  constructor() {
-    super();
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-      test: ''       
-    };
-    console.log('constructor')
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  console.log(searchField);
+  
+  const onSearchChange = (event) => {
+         const searchFieldString = event.target.value.toLocaleLowerCase();
+         setSearchField(searchFieldString);
+         
   }
 
-  //Runs 3rd
-  componentDidMount(){
-    console.log('componentDidMount')
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then((users) => this.setState(()=> {
-        return {monsters: users}
-          },
-          () => {
-            console.log(this.state);
-          }
-        ))    
-  }
-  //Method created in order to optimize app by removing said method from render
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(()=>{
-      return { searchField };
-    })              
-  }
-
-  //Runs 2nd, then runs 4th
-   render() {
-    console.log('render');
-
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
-
-      return (
-          <div className="App">
-            <h1 className='app-title'>The Monsters</h1>
-            <SearchBox 
+  return (
+    <div className="App">
+      <h1 className='app-title'>The Monsters</h1>
+        {
+           <SearchBox 
               className = 'monsters-search-box'  
               onChangeHandler = {onSearchChange} 
               placeholder='search monsters'            
-              />           
-            <Cardlist monsters = {filteredMonsters} className='card-list' />
-          </div>
-      );
-    }
+              />    /*       
+          <Cardlist monsters = {filteredMonsters} className='card-list' /> */
+        }            
+    </div>
+  )
+  
 }
+
+// class App extends Component {
+//   //Runs 1st
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//       test: ''       
+//     };
+//     console.log('constructor')
+//   }
+
+//   //Runs 3rd
+//   componentDidMount(){
+//     console.log('componentDidMount')
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then(response => response.json())
+//       .then((users) => this.setState(()=> {
+//         return {monsters: users}
+//           },
+//           () => {
+//             console.log(this.state);
+//           }
+//         ))    
+//   }
+//   //Method created in order to optimize app by removing said method from render
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(()=>{
+//       return { searchField };
+//     })              
+//   }
+
+//   //Runs 2nd, then runs 4th
+//    render() {
+//     console.log('render');
+
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//       return (
+//           <div className="App">
+//             <h1 className='app-title'>The Monsters</h1>
+//             <SearchBox 
+//               className = 'monsters-search-box'  
+//               onChangeHandler = {onSearchChange} 
+//               placeholder='search monsters'            
+//               />           
+//             <Cardlist monsters = {filteredMonsters} className='card-list' />
+//           </div>
+//       );
+//     }
+// }
 
 export default App;
